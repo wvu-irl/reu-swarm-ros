094e7aa66b9ebbf6047741c1b9b4078f7218e397
Henry Vos <31114971+tntman42@users.noreply.github.com>
2019-05-29 17:20:40 -0400

    Listener added to listen to sockets

---
+++ swarm_server/debug/listener.cpp
@@ -0,0 +1,66 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <time.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define PORT 54321
+#define IP "192.168.10.187"
+
+int main()
+{
+    int socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket_descriptor == -1)
+  {
+    puts("Error getting socket.");
+    exit(1);
+  }
+
+  // Instantiate struct to store socket settings.
+  struct sockaddr_in socket_address;
+  memset(&socket_address, 0, sizeof(socket_address));
+  socket_address.sin_family = AF_INET;
+  socket_address.sin_port = htons(PORT);
+  inet_aton(IP, &(socket_address.sin_addr));
+
+  // Connect to the socket
+  if (connect(socket_descriptor, (struct sockaddr *)&socket_address, sizeof(socket_address)) == -1)
+  {
+    puts("Error connecting to socket.");
+    exit(1);
+  }
+
+  while (true)
+  {
+    // TODO Recieve
+    int message_size = 0;
+    char str[32];
+
+    int count = 0;
+
+    //reading the message
+    while ((message_size = read(socket_descriptor, str, sizeof(str))) > 0)
+    {
+    
+      printf("CLIENT GOT: %s\n", str);
+    }
+
+    // Display if there was an error
+    if (message_size == -1)
+    {
+      
+      puts("Error receiving message.");
+      exit(1);
+    }
+  }
+}
