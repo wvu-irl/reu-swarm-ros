b86d4531279877b2104c5f2cd49afb63902d84eb
nhewitt99 <nhewitt@uncc.edu>
2019-05-30 10:13:06 -0400

    Added tcp demo to ros.

---
+++ swarm_ws/src/swarm_server/src/arduino_test.cpp
@@ -0,0 +1,72 @@
+#include "arduino_server.h"
+#include <sstream>
+#include <string>
+#include <ros/ros.h>
+#include <ros/master.h>
+#include <vicon_demo/rtheta.h>
+
+void commandCallback(command cmd)
+{
+    printf("Robot[%d] head %s\n", 0, "asdg");
+}
+
+void messageCallback(const vicon_demo::rtheta &msg) {}
+
+void info(const char *patt, void *dat)
+{
+    std::ostringstream os;
+    os << "SERVER INFO: " << patt << "\n";
+    std::string full_pattern = os.str();
+    const char *ch_pathh = full_pattern.c_str();
+    printf(ch_pathh, dat);
+}
+
+bool keepalive()
+{
+    return true;
+}
+
+void *sendThread(void *arg0)
+{
+    ros::Subscriber *sub = (ros::Subscriber*)arg0;
+    
+    sleep(30);
+    puts("Sending messages");
+
+    while (true)
+    {
+        vicon_demo::rtheta vector =
+                *(ros::topic::waitForMessage<vicon_demo::rtheta>("/vicon_demo"));
+        
+        //command output = {"0,0.5,135.4"};
+        command output;
+        sprintf(output.str, "%d,%f,%f", 0, vector.radius, vector.degrees);
+        
+        sendCommandToRobots(output);
+        usleep(10000);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    // Set up ros
+    ros::init(argc, argv, "arduino_test");
+    ros::NodeHandle n;
+    ros::Subscriber vectorSub;
+    ros::Rate rate(100);
+    
+    // Subscribe
+    vectorSub = n.subscribe("/vicon_demo", 10, &messageCallback);
+    
+    puts("Starting");
+    printf("Sending %d bytes per message\n", (int)sizeof(command));
+
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_t tid;
+    pthread_create(&tid, &attr, sendThread, &vectorSub);
+
+    beginServer(commandCallback, info, puts, keepalive);
+
+    pthread_join(tid, NULL);
+}
