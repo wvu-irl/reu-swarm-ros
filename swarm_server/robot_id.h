9ba6c90291aad0c4b1bea12b30da3b555277be67
Henry Vos <31114971+tntman42@users.noreply.github.com>
2019-05-31 12:42:31 -0400

    contains conversions for robot ids
    allows for converting between 2 char ids and integer ids

---
+++ swarm_server/robot_id.h
@@ -0,0 +1,23 @@
+#ifndef RID_H
+#define RID_H
+#include <stdlib.h>
+#include <map>
+
+char *rid_indexing[] = {"DE", "PA", "NJ", "GA", "CT", "MA", "MD", "SC", "NH", "VA", "NY", "NC", 
+                                "RI", "VT", "KY", "TN", "OH", "LA", "IN", "MS", "IL", "AL", "ME", "MO", 
+                                "AR", "MI", "FL", "TX", "IA", "WI", "CA", "MN", "OR", "KA", "WV", "NV", 
+                                "NE", "CO", "ND", "SD", "MT", "WA", "ID", "WY", "UT", "OK", "NM", "AZ", 
+                                "AK", "HI"};
+
+std::map<char*, int> inst()
+{
+  std::map<char*, int> map;
+  for (size_t i = 0; i < sizeof(rid_indexing) / sizeof(rid_indexing[0]); i++)
+  {
+    map.insert(std::pair<char*, int>(rid_indexing[i], i));
+  }
+  return map;
+}
+
+std::map<char*, int> rid_map = inst();
+#endif
