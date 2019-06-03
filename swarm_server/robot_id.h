466a0c6e7fd66ddf21448c05f78e8d09b49816e3
tntman42 <tntman42@gmail.com>
2019-06-03 14:58:42 -0400

    added all case and comments

--- swarm_server/robot_id.h
+++ swarm_server/robot_id.h
@@ -4,6 +4,7 @@
 #include <map>
 #include <string>
 
+// generic dataset for all the states
 std::string rid_indexing[] = {"DE", "PA", "NJ", "GA", "CT", "MA", "MD", "SC", "NH", "VA", "NY", "NC",
                                 "RI", "VT", "KY", "TN", "OH", "LA", "IN", "MS", "IL", "AL", "ME", "MO",
                                 "AR", "MI", "FL", "TX", "IA", "WI", "CA", "MN", "OR", "KA", "WV", "NV",
@@ -12,13 +13,14 @@ std::string rid_indexing[] = {"DE", "PA", "NJ", "GA", "CT", "MA", "MD", "SC", "N
 
 std::map<std::string, int> initializeRIDs()
 {
-  std::map<std::string, int> map;
-  for (size_t i = 0; i < sizeof(rid_indexing) / sizeof(rid_indexing[0]); i++) {
+  std::map<std::string, int> map; // map containing all the conversions
+  for (size_t i = 0; i < sizeof(rid_indexing) / sizeof(rid_indexing[0]); i++) { // adding all states in order from 0-49
     map.insert(std::pair<std::string, int>(rid_indexing[i], i));
   }
+  map.insert("XX", -1); // sending to all case
   return map;
 }
 
-std::map<std::string, int> rid_map = initializeRIDs();
+std::map<std::string, int> rid_map = initializeRIDs(); // making default mapp
 
 #endif
