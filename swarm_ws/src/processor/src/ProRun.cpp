b580997834509f0b11f90dfbc94568c4e10abe47
BlackfyreHG <50957256+BlackfyreHG@users.noreply.github.com>
2019-06-04 10:39:19 -0400

    Jeongwoo updates

--- swarm_ws/src/processor/src/ProRun.cpp
+++ swarm_ws/src/processor/src/ProRun.cpp
@@ -12,22 +12,26 @@ void botCallback(const wvu_swarm_std_msgs::viconBotArray &msg)
 
 int main(int argc, char **argv)
 {
-  /* Test code for neighbor finding
-  // Bot a(0, 0, "ab");
-  //  Bot b(1, 1, "bc");
-  //  Bot c(2, 2, "cd");
-  //  Bot d(3, 3, "de");
-  //  Bot e(4, 4, "ef");
-  //  Bot f(5, 5, "fg");
-  //  Bot g(6, 6, "gh");
-  //  Bot h(7, 7, "hi");
-  //  Bot i(8, 8, "ij");
-  //  Bot j(9, 9, "jk");
-  //  Bot inputList [10] = {a, b, c, d, e, f, g, h, i, j};
-  //  Processor test_pros = Processor(inputList);
-  //  test_pros.findNeighbors();
-  //  test_pros.printBotMail();
-  */
+  // Test code for neighbor finding
+  Bot a(0, 0, "ab");
+   Bot b(1, 1, "bc");
+   Bot c(2, 2, "cd");
+   Bot d(3, 3, "de");
+   Bot e(4, 4, "ef");
+   Bot f(5, 5, "fg");
+   Bot g(6, 6, "gh");
+   Bot h(7, 7, "hi");
+   Bot i(8, 8, "ij");
+   Bot j(9, 9, "jk");
+   Bot inputList [10] = {a, b, c, d, e, f, g, h, i, j};
+   Processor test_pros = Processor(inputList);
+   test_pros.findNeighbors();
+   test_pros.printBotMail();
+   wvu_swarm_std_msgs::aliceMailArray msg = test_pros.createAliceMsg(9);
+   test_pros.printAliceMail(msg);
+   std::cout << std::endl;
+
+
   Processor bigbrain(0);
   ros::init(argc, argv, "Processor");
   ros::NodeHandle n;
@@ -36,7 +40,7 @@ int main(int argc, char **argv)
 
   for (int i = 0; i < BOT_COUNT; i++) //Starts publishing to all 50 topics
   {
-    ros::Publisher pub = n.advertise<std_msgs::String>("alice_mail_" + std::to_string(i), 1000);
+    ros::Publisher pub = n.advertise<wvu_swarm_std_msgs::aliceMailArray>("alice_mail_" + std::to_string(i), 1000);
     pubVector.push_back(pub);
   }
 
@@ -47,7 +51,20 @@ int main(int argc, char **argv)
 
     bigbrain.processVicon(tempBotArray);
     bigbrain.findNeighbors();
-
+    // Bot a(0, 0, "ab");
+    //  Bot b(1, 1, "bc");
+    //  Bot c(2, 2, "cd");
+    //  Bot d(3, 3, "de");
+    //  Bot e(4, 4, "ef");
+    //  Bot f(5, 5, "fg");
+    //  Bot g(6, 6, "gh");
+    //  Bot h(7, 7, "hi");
+    //  Bot i(8, 8, "ij");
+    //  Bot j(9, 9, "jk");
+    //  Bot inputList [10] = {a, b, c, d, e, f, g, h, i, j};
+    //  Processor test_pros = Processor(inputList);
+    //  test_pros.findNeighbors();
+     std::cout << "x"
     for (int i = 0; i < BOT_COUNT; i++) //Publishes msgs to Alices
     {
       pubVector.at(i).publish(bigbrain.createAliceMsg(i));
