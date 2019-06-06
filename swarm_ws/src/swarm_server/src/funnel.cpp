471a1dba2226ec7c3fab13fb081934cc6f5974bf
Henry Vos <tntman42@gmail.com>
2019-06-06 15:29:12 -0400

    This was less code

--- swarm_ws/src/swarm_server/src/funnel.cpp
+++ swarm_ws/src/swarm_server/src/funnel.cpp
@@ -37,10 +37,7 @@ void *listeningThread(void *arg0)
 	std::string topic; // constructing topic name
 	char temp_topic_string[16] =
 	{ '\0' };
-	if (id > 9)
-		sprintf(temp_topic_string, "execute_%d", id);
-	else
-		sprintf(temp_topic_string, "execute_0%d", id);
+	sprintf(temp_topic_string, "execute_%02d", id);
 	topic = std::string(temp_topic_string);
 	ROS_INFO("Subscribing to : %s", topic.c_str()); // checking
 	ros::Subscriber exe = n.subscribe(topic, 1000, compressionCallback); // subscribing to topic
