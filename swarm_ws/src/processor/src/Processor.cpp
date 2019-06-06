6d8e77690260a221913e2820f90527c8651f41a0
jeoseo <jeongwoo.seo@vanderbilt.edu>
2019-06-06 10:17:25 -0400

    got rid of wasteful memory stuffz and formatted.

--- swarm_ws/src/processor/src/Processor.cpp
+++ swarm_ws/src/processor/src/Processor.cpp
@@ -10,14 +10,13 @@
 #include <math.h>
 #include <swarm_server/robot_id.h>
 
-std::pair<float, float> Processor::getSeparation(Bot _bot,
-		std::pair<float, float> _obs, float _tolerance) //helper function for finding obstacle points.
+wvu_swarm_std_msgs::obs_point_mail Processor::getSeparation(Bot _bot, std::pair<float, float> _obs, float _tolerance) //helper function for finding obstacle points.
 { // takes current bot and looks at distance to each obs point. If it "sees" it, converts that obs to polar and pushes to its vector stored in polar_obs.
 	float loc_r; //|distance| b/w bot and current obstacle point.
 	float theta; //in radians
 	float dx; //x separation.
 	float dy; //y separation.
-
+	wvu_swarm_std_msgs::obs_point_mail polar_point;
 	dx = _obs.first - _bot.x;
 	dy = _obs.second - _bot.y;
 	loc_r = sqrt(pow(dx, 2) + pow(dy, 2)); //magnitude of separation
@@ -37,13 +36,13 @@ std::pair<float, float> Processor::getSeparation(Bot _bot,
 			theta = 2 * M_PI + theta;
 		}
 
-		std::pair<float, float> polar_point(loc_r, theta);
-		//std::cout<<"____"<<polar_point.first<<","<<polar_point.second<<"________"<<"\n";
+		polar_point.radius = loc_r;
+		polar_point.theta = theta;
 		return polar_point;
 	} else
 	{
-		//std::cout<<"returns NULL (a negative r value)"<<"\n";
-		std::pair<float, float> polar_point(-1, 0); //*((std::pair<float, float> *) NULL);
+		polar_point.radius = -1;
+		polar_point.theta = 0;
 		return polar_point;
 	}
 }
@@ -82,8 +81,7 @@ void Processor::processVicon(wvu_swarm_std_msgs::vicon_bot_array data) //Fills i
 
 	for (size_t i = 0; i < data.poseVect.size(); i++)
 	{
-		char bid[3] =
-		{ '\0' };
+		char bid[3] = { '\0' };
 		bid[0] = data.poseVect[i].botId[0];
 		bid[1] = data.poseVect[i].botId[1];
 		std::string tempID(bid);
@@ -107,8 +105,8 @@ void Processor::printBots() //Prints the
 	using namespace std;
 	for (int i = 0; i < BOT_COUNT; i++)
 	{
-		cout << "[" << bots[i].id[0] << bots[i].id[1] << " " << bots[i].x << " "
-				<< bots[i].y << " " << bots[i].heading << " ";
+		cout << "[" << bots[i].id[0] << bots[i].id[1] << " " << bots[i].x << " " << bots[i].y << " " << bots[i].heading
+				<< " ";
 		cout << bots[i].distance << "]\n";
 	}
 }
@@ -118,7 +116,7 @@ void Processor::printBotMail() //Prints the id's in botMail[] to the console
 	using namespace std;
 	for (int i = 0; i < BOT_COUNT; i++)
 	{
-		std::cout << "=========== bots "<<i<<" Neighbors ========" << "\n";
+		std::cout << "=========== bots " << i << " Neighbors ========" << "\n";
 		std::cout << "[";
 		for (int j = 0; j < NEIGHBOR_COUNT; j++)
 		{
@@ -127,14 +125,8 @@ void Processor::printBotMail() //Prints the id's in botMail[] to the console
 			cout << " ";
 		}
 		cout << "]\n";
-		std::cout << "++++++++++bots "<<i<<" obstacles++++++" << "\n";
-		std::cout << "[";
-		for (int k = 0; k < polar_obs[i].size(); k++)
-		{
-			cout << "|" << polar_obs[i].at(k).first << ","
-					<< polar_obs[i].at(k).second << "|";
-		}
-		cout << "]\n";
+		std::cout << "++++++++++bots " << i << " obstacles++++++" << "\n";
+
 	}
 	std::cout << "------------------------" << "\n";
 }
@@ -156,12 +148,11 @@ void Processor::printAliceMail(wvu_swarm_std_msgs::alice_mail_array _msg)
 	for (int j = 0; j < _msg.obsPointMail.size(); j++)
 	{
 		cout << "|";
-		cout <<_msg.obsPointMail[j].radius<<","<<_msg.obsPointMail[j].theta;
+		cout << _msg.obsPointMail[j].radius << "," << _msg.obsPointMail[j].theta;
 		cout << "|";
 	}
 	cout << "]\n";
 }
-
 
 /*
  Exactly what it sounds like
@@ -171,32 +162,8 @@ void Processor::printAliceMail(wvu_swarm_std_msgs::alice_mail_array _msg)
 void Processor::findNeighbors()
 {
 	int botIndex = 0;
-	int j; //iterator for obs finding loop
-
-	int num_pts = static_cast<int>(obs.size());
-	std::pair<float, float> new_pair; //holds the return pair
-	float tolerance = 12; //this value is supposed to be our actual tolerance (just made it 12).
-	//holds obs near a robot for each robot.
-	//probably not our long term solution for storing the data.
-
 	for (auto &bot : bots)
 	{
-		j = 0;
-		while (j < num_pts) //runs for each point in the obs
-		{
-			std::pair<float, float> iter_obs = obs.at(j); //current obs to test
-			new_pair = getSeparation(bot, iter_obs, tolerance);
-			if (new_pair.first > -1)
-			{
-				polar_obs[botIndex].push_back(new_pair);
-				//std::cout<<polar_obs[botIndex].back().first<<","<<polar_obs[botIndex].back().second " was pushed"<<"\n";
-			}
-			/*else {
-			 std::cout<<"---- nothing pushed ----"<<"\n";
-			 }*/
-			j++;
-		}
-
 		int curIndex = 0; // Because we're looping over the array, we have to track the index ourselves
 		for (auto &cur : bots)
 		{
@@ -220,8 +187,7 @@ void Processor::findNeighbors()
 					{ // This means cur is neither the furthest nor nearest
 						botMail[botIndex][0] = temp;
 						smallest = false;
-						std::sort(botMail[botIndex], botMail[botIndex] + NEIGHBOR_COUNT,
-								compareTwoBots); // Sorts greatest first
+						std::sort(botMail[botIndex], botMail[botIndex] + NEIGHBOR_COUNT, compareTwoBots); // Sorts greatest first
 						break;
 					}
 				}
@@ -229,54 +195,57 @@ void Processor::findNeighbors()
 			if (smallest)
 			{ // If cur is closer than everything, it's the closest
 				botMail[botIndex][0] = temp;
-				std::sort(botMail[botIndex], botMail[botIndex] + NEIGHBOR_COUNT,
-						compareTwoBots); // Sorts greatest first
+				std::sort(botMail[botIndex], botMail[botIndex] + NEIGHBOR_COUNT, compareTwoBots); // Sorts greatest first
 			}
 			curIndex++;
 		}
-
 		botIndex++;
 	}
 }
 
+wvu_swarm_std_msgs::neighbor_mail Processor::createNeighborMail(int i, int j)
+{
+	wvu_swarm_std_msgs::neighbor_mail _neighborMail;
+
+	if (botMail[i][j].y - bots[i].y > 0)
+	{
+		_neighborMail.theta = fmod(
+				atan((botMail[i][j].y - bots[i].y) / (botMail[i][j].x - bots[i].x)) - M_PI_2 - bots[i].heading, 2 * M_PI);
+	} else
+	{
+		_neighborMail.theta = fmod(
+				atan((botMail[i][j].y - bots[i].y) / (botMail[i][j].x - bots[i].x)) + M_PI_2 - bots[i].heading, 2 * M_PI);
+	}
+	_neighborMail.distance = botMail[i][j].distance;
+	_neighborMail.heading = fmod(botMail[i][j].heading - bots[i].heading, 2 * M_PI);
+
+	for (int j = 0; j < NEIGHBOR_COUNT; j++)
+	{
+		botMail[i][j] = Bot();
+	}
+	return _neighborMail;
+}
+
 wvu_swarm_std_msgs::alice_mail_array Processor::createAliceMsg(int i) //Turns information to be sent to Alice into a msg
 {
-	int num_pts = polar_obs[i].size(); //number of obs pts near this bot.
 
 	wvu_swarm_std_msgs::alice_mail_array _aliceMailArray;
 
-	//std::cout<<"size of obsPointMail = "<<_aliceMailArray.obsPointMail.size()<<"\n";
+	int num_pts = obs.size(); //number of obs pts
+	for (int j = 0; j < num_pts; j++)
+	{
+		wvu_swarm_std_msgs::obs_point_mail _obsPointMail = getSeparation(bots[i], obs.at(j), TOLERANCE);
+		if (_obsPointMail.radius > -1)
+		{
+			_aliceMailArray.obsPointMail.push_back(_obsPointMail);
+		}
+	}
 
 	for (int j = 0; j < NEIGHBOR_COUNT; j++) //Transfers fields of the struct to fields of the msg
 	{
-		if ((j < num_pts) && (num_pts >0))
-		{
-			wvu_swarm_std_msgs::obs_point_mail current;
-			current.radius = polar_obs[i].at(j).first;
-			current.theta =  polar_obs[i].at(j).second;
-			_aliceMailArray.obsPointMail.push_back(current);
-		}
-
-		if (botMail[i][j].y - bots[i].y > 0)
-		{
-			_aliceMailArray.neighborMail[j].theta = fmod(
-					atan((botMail[i][j].y - bots[i].y) / (botMail[i][j].x - bots[i].x))
-							- M_PI_2 - bots[i].heading, 2 * M_PI);
-		} else
-		{
-			_aliceMailArray.neighborMail[j].theta = fmod(
-					atan((botMail[i][j].y - bots[i].y) / (botMail[i][j].x - bots[i].x))
-							+ M_PI_2 - bots[i].heading, 2 * M_PI);
-		}
-		_aliceMailArray.neighborMail[j].distance = botMail[i][j].distance;
-		_aliceMailArray.neighborMail[j].heading = fmod(
-				botMail[i][j].heading - bots[i].heading, 2 * M_PI);
-
-		for (int j= 0; j< NEIGHBOR_COUNT; j++){
-		         botMail[i][j] = Bot();
-		      }
-
+		_aliceMailArray.neighborMail[j] = createNeighborMail(i, j);
 	}
+
 	return _aliceMailArray;
 
 }
