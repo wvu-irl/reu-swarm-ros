cb73fa286a4be28a93728a883472bf856928efe6
TheVioletBaron <46730679+TheVioletBaron@users.noreply.github.com>
2019-06-06 10:48:31 -0400

    Fixed string length issues

--- swarm_ws/src/alice_pkg/src/test_package/Robot.py
+++ swarm_ws/src/alice_pkg/src/test_package/Robot.py
@@ -16,23 +16,23 @@ def callback(data):
 	#Alice.queueUpdate(data)
 
 class Robot:
-	running = True
 	speed = 0.8
 	angle = 0
 	name = 'Alice'
 	vector_queue = VectorQueue() #create queue
+        model = Model(speed)
 	MAILBOX = 7 #Sets the number of messeges to hold
 
 	def callToVector(self, data):
-		vector_queue.queueUpdate(data)
+		self.vector_queue.queueUpdate(data)
 
 	def callToModel(self, data):
- 		model.modelUpdate(data)
+ 		self.model.modelUpdate(data)
 
 	def __init__(self, name):
-		
 		rospy.init_node(name, anonymous=False)	
-		self.name = rospy.get_param('~id')
+		self.name = '%02d' % int(rospy.get_param('~id'))
+		print(self.name)
 		ideal_pub = rospy.Publisher('ideals', Float64MultiArray, queue_size = self.MAILBOX)
 		ex_string = "execute_" + self.name
 		execute_pub = rospy.Publisher(ex_string, robot_command, queue_size = self.MAILBOX)
@@ -41,28 +41,24 @@ class Robot:
 		rospy.Subscriber(sub_string, alice_mail_array, self.callToModel)
 		
 		self.rate = rospy.Rate(10)
-		while self.running == True:
-			model = Model(self.speed)
+		while not rospy.is_shutdown():
+			self.model = Model(self.speed)
 			#run loops to update model
 			#model.addObstacle((math.pi/2, 15))
 			#model.addObstacle((4 * math.pi/7 , 30))
-			model.addRobot((math.pi/2, 10, 0.9, math.pi/2))
-			model.addRobot((3*math.pi/2, 10, 0.7, math.pi/2))
+			self.model.addRobot((math.pi/2, 10, 0.9, math.pi/2))
+			self.model.addRobot((3*math.pi/2, 10, 0.7, math.pi/2))
 	
 			#accept sensor input to model
-			self.vector_queue.addVector(model.generateIdeal())
+			self.vector_queue.addVector(self.model.generateIdeal())
 			compromise_vector = self.vector_queue.createCompromise()
-			print (compromise_vector)
 	
-			ideal = Float64MultiArray(data=model.generateIdeal())
+			ideal = Float64MultiArray(data=self.model.generateIdeal())
 			ideal_pub.publish(ideal)
 			compromise = robot_command()
 			compromise.rid = list(self.name)
 			compromise.theta = float(compromise_vector[0])
 			compromise.r = float(compromise_vector[1])
 			execute_pub.publish(compromise)
-			rospy.spin()
 			self.rate.sleep()
-
-			self.running = False #breaks loop in tests
 
