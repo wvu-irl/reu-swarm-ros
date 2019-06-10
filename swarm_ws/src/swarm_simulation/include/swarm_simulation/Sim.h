a9a10900c1ebcded429d868e80b33381e18eb602
Henry Gunner <smart3@smart3>
2019-06-10 16:50:26 -0400

    added sub to sim

--- swarm_ws/src/swarm_simulation/include/swarm_simulation/Sim.h
+++ swarm_ws/src/swarm_simulation/include/swarm_simulation/Sim.h
@@ -5,6 +5,7 @@
 #include "SFML/Window.hpp"
 #include "SFML/Graphics.hpp"
 #include <ros/ros.h>
+#include <wvu_swarm_std_msgs/robot_command_array.h>
 //#include <swarm_server/robot_id.h>
 
 #ifndef SIM_H
@@ -19,7 +20,7 @@ private:
     sf::RenderWindow window;
     int window_width;
     int window_height;
-
+    void vectorCallback(const wvu_swarm_std_msgs::robot_command_array &msg);
     Flock flock;
     float bodiesSize;
     vector<sf::CircleShape> shapes;
