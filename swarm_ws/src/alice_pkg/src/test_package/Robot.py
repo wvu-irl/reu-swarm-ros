33be7a002bf7db3f944101e1e96ba5872715a607
TheVioletBaron <46730679+TheVioletBaron@users.noreply.github.com>
2019-06-12 10:42:29 -0400

    Reuploaded

--- swarm_ws/src/alice_pkg/src/test_package/Robot.py
+++ swarm_ws/src/alice_pkg/src/test_package/Robot.py
@@ -3,8 +3,10 @@
 
 from VectorQueue import VectorQueue
 from Model import Model
+from SelfPortrait import SelfPortrait
 import math
 import rospy
+import Tkinter
 from std_msgs.msg import Float64MultiArray
 from std_msgs.msg import String
 from wvu_swarm_std_msgs.msg import robot_command
@@ -21,6 +23,8 @@ class Robot:
 	name = 'Alice'
 	vector_queue = VectorQueue() #create queue
 	model = Model(speed)
+	compromise_vector = None
+	ideal_vector = None
 	MAILBOX = 7 #Sets the number of messeges to hold
 
 	def callToVector(self, data):
@@ -31,21 +35,21 @@ class Robot:
 
 	def setUpNode(self):
 		rospy.init_node(self.name, anonymous=False)	
-		self.name = int(rospy.get_param('~id')
+		self.name = int(rospy.get_param('~id'))
 		self.ideal_pub = rospy.Publisher('ideals', Float64MultiArray, queue_size = self.MAILBOX)
-		ex_string = "execute_" + self.name
+		ex_string = "execute_" + str(self.name)
 		self.execute_pub = rospy.Publisher(ex_string, robot_command, queue_size = self.MAILBOX)
-		rospy.Subscriber("ideals", Float64MultiArray, callback)
-		sub_string = "alice_mail_" + self.name
+		rospy.Subscriber("ideals", Float64MultiArray, self.callToVector)
+		sub_string = "alice_mail_" + str(self.name)
 		rospy.Subscriber(sub_string, alice_mail_array, self.callToModel)
 		self.rate = rospy.Rate(10)
-
+	'''
 	def tester(self):
 		self.model.addObstacle((math.pi/2, 15))
 		self.model.addObstacle((4 * math.pi/7 , 30))
 		self.model.addRobot((math.pi/2, 10, 0.9, math.pi/2))
 		self.model.addRobot((3*math.pi/2, 10, 0.7, math.pi/2))
-
+	'''
 	def publishData(self):
 		ideal = Float64MultiArray(data=self.model.generateIdeal())
 		self.ideal_pub.publish(ideal)
@@ -53,18 +57,16 @@ class Robot:
 		compromise.rid = self.name
 		compromise.theta = float(self.compromise_vector[0])
 		compromise.r = float(self.compromise_vector[1])
+		self.speed = compromise.r #Should eventually be updated
 		self.execute_pub.publish(compromise)
 		
 
 	def __init__(self, name):
 		
 		self.setUpNode()
-
 		self_portrait = SelfPortrait(self)
 		should_update = True
-
 		while not rospy.is_shutdown():
-
 			if should_update:
 				try:
 					self_portrait.root.update()
@@ -73,9 +75,9 @@ class Robot:
 			
 			self.model = Model(self.speed)
 	
-			self.tester() #Uncomment to include test inputs
-			
-			self.vector_queue.addVector(self.model.generateIdeal())
+			#self.tester() #Uncomment to include test inputs
+			self.ideal_vector = self.model.generateIdeal()
+			self.vector_queue.addVector(self.ideal_vector)
 			self.compromise_vector = self.vector_queue.createCompromise()
 	
 			self.publishData()
--- swarm_ws/src/alice_pkg/src/test_package/SelfPortrait.py
+++ swarm_ws/src/alice_pkg/src/test_package/SelfPortrait.py
@@ -1,24 +1,58 @@
-#from graphics import *
-import tkinter
-from tkinter import *
+from Tkinter import *
+import Tkinter
 import sys
-
+import Robot
+		
 class SelfPortrait:
 	tolerance = 0
 	speed = 0
+	name = "Alice"
+	compromise = ()
+	ideal = ()
+	robot = None
 	root = Tk()
 	root.title("Alice Self-Portrait")
+	_compromise = None
 	
-	refresh = Button(root, text = "Click to refresh")
-	Button.grid(column = 1, row = 1)
+	def call_refresh(self):
+		self.tolerance = self.robot.model.tolerance
+		self.name = self.robot.name
+		self.speed = self.robot.speed
+		self.compromise = self.robot.compromise_vector
+		self.ideal = self.robot.ideal_vector
+		
+		self._compromise = Label(self.root, text = "Compromise vector: " + str(self.compromise))
+		self._compromise.grid(column = 1, row = 2)
+		
+		self._ideal = Label(self.root, text = "Ideal vector: " + str(self.ideal))
+		self._ideal.grid(column = 1, row = 3)
+		
 	
-	_tolerance = Label(root, text = "Tolerance is: " + str(tolerance))
-	_tolerance.grid(column = 0, row = 2)
+	def __init__(self, robot):
+		self.robot = robot
+		self.tolerance = self.robot.model.tolerance
+		self.name = self.robot.name
+		self.speed = self.robot.speed
+		self.compromise = self.robot.compromise_vector
+		self.ideal = self.robot.ideal_vector
+		
+		
+		refresh = Button(self.root, text = "Click to refresh", command = self.call_refresh)
+		refresh.grid(column = 1, row = 1)
 	
-	_speed = Label(root, text = "Speed is: " + str(speed))
-	_speed.grid(column = 0, row = 3)
+		_tolerance = Label(self.root, text = "Tolerance is: " + str(self.tolerance))
+		_tolerance.grid(column = 0, row = 2)
 	
+		_speed = Label(self.root, text = "Speed is: " + str(self.speed))
+		_speed.grid(column = 0, row = 3)
+	
+		_name = Label(self.root, text = self.name)
+		_name.grid(column = 1, row = 0)
 
-	root.mainloop()	
-
-Alice = SelfPortrait()
+		self._compromise = Label(self.root, text = "Compromise vector: " + str(self.compromise))
+		self._compromise.grid(column = 1, row = 2)
+		
+		self._ideal = Label(self.root, text = "Ideal vector: " + str(self.ideal))
+		self._ideal.grid(column = 1, row = 3)
+	
+		#_compromise = Label(root, text = 
