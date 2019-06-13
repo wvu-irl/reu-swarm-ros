b846999ea1beb04937d14f05eeba5142f0d18ad3
Henry Vos <tntman42@gmail.com>
2019-06-13 09:16:27 -0400

    Created visualizer package

---
+++ swarm_ws/src/visualization/src/color_map.h
@@ -0,0 +1,21 @@
+#ifndef COLOR_MAP_H
+#define COLOR_MAP_H
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+class ColorMap
+{
+private:
+	std::vector<std::tuple<double, sf::Color>> colors;
+
+public:
+	ColorMap(std::tuple<double, sf::Color> min,
+			std::tuple<double, sf::Color> max);
+
+	void addColor(std::tuple<double, sf::Color>);
+	sf::Color calculateColor(double val);
+};
+
+#include "color_map.cpp"
+#endif
---
+++ swarm_ws/src/visualization/src/contour.h
@@ -0,0 +1,66 @@
+#ifndef CONTOUR_H
+#define CONTOUR_H
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "color_map.h"
+#include <functional>
+
+#define LINE_CHECK_DIST 1
+
+class Vector3D
+{
+public:
+	double x, y, z;
+
+	Vector3D(double x, double y, double z);
+
+	double magnitude() const;
+
+	double dot(const Vector3D &rhs) const;
+
+	Vector3D operator-(const Vector3D &rhs) const
+	{
+		return Vector3D(x - rhs.x, y - rhs.y, z - rhs.z);
+	}
+
+	Vector3D operator*(double rhs) const
+	{
+		return Vector3D(rhs * x, rhs * y, rhs * z);
+	}
+
+	friend std::ostream &operator<<(std::ostream &, const Vector3D &);
+};
+
+class ContourMap
+{
+private:
+	sf::Uint8 *cols;
+	sf::Image img;
+	sf::Texture tex;
+	sf::Sprite sprite;
+
+	std::function<double(double, double)> zfunc;
+
+	ColorMap color_mapping;
+
+public:
+	sf::Rect<int> bounds;
+	std::vector<double> levels;
+
+	ContourMap(sf::Rect<int> bounds, ColorMap);
+
+	void tick();
+	void render(sf::RenderWindow *window);
+
+	void resemble(std::function<double(double, double)> z);
+
+	void scale(float sx, float sy);
+
+	void setColorMap(ColorMap);
+	ColorMap *getColorMap();
+
+	~ContourMap();
+};
+#include "contour.cpp"
+#endif
