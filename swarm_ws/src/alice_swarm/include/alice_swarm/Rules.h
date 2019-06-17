cff9c0040c870bacc10272fabe1dfa888eaff1a1
TheVioletBaron <cedmonds2018@gmail.com>
2019-06-17 09:59:32 -0400

    added panicAvoid

--- swarm_ws/src/alice_swarm/include/alice_swarm/Rules.h
+++ swarm_ws/src/alice_swarm/include/alice_swarm/Rules.h
@@ -4,7 +4,8 @@
 #include "alice_swarm/aliceStructs.h"
 #include "math.h"
 
-static 	const float ROBOT_SIZE = 7; //Size of the robot, in cm
+static const float ROBOT_SIZE = 7; //Size of the robot, in cm
+//static const int SENSE = 1; //Crank this up to improve accuracy, but takes more computational power
 
 class Rules
 {
@@ -14,11 +15,11 @@ public:
 
 	AliceStructs::vel maintainSpacing(std::list <AliceStructs::neighbor> bots, float tolerance);
 
-	AliceStructs::vel avoidRobots(std::list <AliceStructs::neighbor> bots, float tolerance);
+	AliceStructs::vel predictiveAvoid(std::list <AliceStructs::neighbor> bots, float tolerance);
 
 	AliceStructs::vel avoidObstacles(std::list <AliceStructs::obj> obstacles, float tolerance);
 
-
+	AliceStructs::vel panicAvoid(std::list <AliceStructs::neighbor> bots, float tolerance);
 	//vel goToTarget(std::list <obs> targets, float tolerance); to implement later
 
 private:
