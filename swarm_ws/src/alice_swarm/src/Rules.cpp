942b34aaabef44abcd7605020e9f5f443dca4b3a
TheVioletBaron <cedmonds2018@gmail.com>
2019-07-03 15:37:27 -0400

    Within striking distance of compiling

--- swarm_ws/src/alice_swarm/src/Rules.cpp
+++ swarm_ws/src/alice_swarm/src/Rules.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <math.h>
 #include <iostream>
+#include <bits/stdc++.h>
 
 Rules::Rules()
 {
@@ -51,13 +52,13 @@ AliceStructs::vel Rules::stateLoop()
 
 void Rules::avoidCollisions()
 {
-	float tf = atan2(model.goTo.y/model.goTo.x);
+	float tf = atan2(model.goTo.y, model.goTo.x);
 	std::vector<std::pair<float, float>> dead_zones = findDeadZones();
 	findAngle(tf, dead_zones);
-	sort(testers.begin, testers.end);
-	final_vel.dir = testers.front;
+	sort(testers.begin(), testers.end());
+	final_vel.dir = testers.front();
 	final_vel.mag = 1;
-	if (final_vel.dir == atan2(model.goTo.y/model.goTo.x))
+	if (final_vel.dir == atan2(model.goTo.y, model.goTo.x))
 	{
 		state = "explore";
 	}
@@ -75,7 +76,7 @@ void Rules::Explore()
 			model.goTo = tar;
 		}
 	}
-	checkBlocked;
+	checkBlocked();
 	final_vel.dir = atan2(model.goTo.y, model.goTo.x);
 	final_vel.mag = 1;
 }
@@ -102,13 +103,13 @@ float Rules::calcDis(float _x1, float _y1, float _x2, float _y2)
 	return pow(pow(_x1 - _x2, 2) + pow(_y1 - _y2, 2), 0.5);
 }
 
-bool checkBlocked()
+bool Rules::checkBlocked()
 {
-	std::vector<std::pair<float, float>> dead_zones = findDeadZones;
-	for (auto& zone : findDeadZones)
+	std::vector<std::pair<float, float>> dead_zones = findDeadZones();
+	for (auto& zone : findDeadZones())
 	{
-		if (zone.first < atan2(model.goTo.y/model.oTo.x) < zone.second ||
-				zone.first > atan2(model.goTo.y/model.oTo.x) > zone.second)
+		if (zone.first < atan2(model.goTo.y, model.goTo.x) < zone.second ||
+				zone.first > atan2(model.goTo.y, model.goTo.x) > zone.second)
 		{
 			state = "blocked";
 			return true;
@@ -139,16 +140,16 @@ void Rules::findAngle(float tf, std::vector<std::pair<float, float>> dead_zones)
 	}
 	else
 	{
-		right.sort();
-		left.sort();
-		find_angle(right.begin, dead_zones);
-		find_angle(left.begom, dead_zones);
+		sort(right.begin(), right.end());
+		sort(left.begin(), right.end());
+		findAngle(right.front(), dead_zones);
+		findAngle(left.front(), dead_zones);
 	}
 }
 
 std::vector<std::pair<float, float>> Rules::findDeadZones()
 {
-	float tf = atan2(model.goTo.y/model.goTo.x);
+	float tf = atan2(model.goTo.y, model.goTo.x);
 	std::vector<std::pair<float, float>> dead_zones;
 	for (auto& obs : model.obstacles)
 	{
