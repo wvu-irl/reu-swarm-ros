539b0230d7af4c0e28b368c1f528b6857c91fee2
jeoseo <jeongwoo.seo@vanderbilt.edu>
2019-07-03 15:57:55 -0400

    exec made

--- swarm_ws/src/alice_swarm/src/AliceMain.cpp
+++ swarm_ws/src/alice_swarm/src/AliceMain.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
-#include "wvu_swarm_std_msgs/alice_mail.h"
+#include "wvu_swarm_std_msgs/alice_mail_array.h"
+#include "wvu_swarm_std_msgs/robot_command_array.h"
 #include "alice_swarm/Hub.h"
 #include "alice_swarm/Alice.h"
 #include "alice_swarm/aliceStructs.h"
@@ -7,58 +8,36 @@
 #include <map>
 #include <chrono>
 
-//wvu_swarm_std_msgs::alice_mail_array temp_mail;
-//
-//void aliceCallback(const wvu_swarm_std_msgs::alice_mail_array &msg)
-//{
-//	temp_mail=msg;
-//}
+wvu_swarm_std_msgs::alice_mail_array temp_mail;
+
+void aliceCallback(const wvu_swarm_std_msgs::alice_mail_array &msg)
+{
+	temp_mail=msg;
+}
 
 int main(int argc, char **argv)
 {
-
-	wvu_swarm_std_msgs::alice_mail mail;
-	wvu_swarm_std_msgs::point_mail tar;
-	tar.x = 20;
-	tar.y = 0;
-	wvu_swarm_std_msgs::ellipse obj;
-	obj.x_rad = 5;
-	obj.y_rad = 5;
-	obj.theta_offset = 0;
-	obj.offset_x = 10;
-	obj.offset_y = 10;
-	mail.obsMail.push_back(obj);
-	mail.targetMail.push_back(tar);
-	Alice alice;
-	alice = Alice(mail);
-	alice.generateVel();
-	//std::map<int, Robot> aliceMap; //Maps robot id's to robots so they can be accessed easily
+	std::map<int, Alice> aliceMap; //Maps robot id's to robots so they can be accessed easily
 
 	//Creates an AliceBrain node, and subscribes/publishes to the necessary topics
-//	ros::init(argc, argv, "AliceBrain");
-//	ros::NodeHandle n;
-//	ros::Subscriber sub = n.subscribe("alice_mail_array", 1000, aliceCallback);
-//	ros::Rate loopRate(200);
-//	ros::Publisher pub = n.advertise < wvu_swarm_std_msgs::robot_command_array > ("final_execute", 1000);
-	/*
+	ros::init(argc, argv, "AliceBrain");
+	ros::NodeHandle n;
+	ros::Subscriber sub = n.subscribe("alice_mail_array", 1000, aliceCallback);
+	ros::Rate loopRate(10);
+	ros::Publisher pub = n.advertise < wvu_swarm_std_msgs::robot_command_array > ("final_execute", 1000);
+
 	while (ros::ok())
 	{
 		auto start = std::chrono::high_resolution_clock::now(); //timer for measuring the runtime of Alice
 		for (int i = 0; i < temp_mail.mails.size(); i++)
 		{
-			aliceMap[temp_mail.mails.at(i).name].receiveMsg(temp_mail.mails.at(i)); //gives each robot the relative data it needs
-		}
-		std::vector<AliceStructs::ideal> all_ideals; //Creates a vector that stores the robot's initial ideal vectors
-		for (std::map<int, Robot>::iterator it = aliceMap.begin(); it != aliceMap.end(); ++it) //eventually run this part asynchronously
-		{
-
-			all_ideals.push_back(it->second.generateIdeal()); //Adds the ideals to the vector
+			aliceMap[temp_mail.mails.at(i).name].updateModel(temp_mail.mails.at(i)); //gives each robot the relative data it needs
 		}
 		wvu_swarm_std_msgs::robot_command_array execute;
-		for (std::map<int, Robot>::iterator it = aliceMap.begin(); it != aliceMap.end(); ++it) //eventually run this part asynchronously
+		for (std::map<int, Alice>::iterator it = aliceMap.begin(); it != aliceMap.end(); ++it) //eventually run this part asynchronously
 		{
 			wvu_swarm_std_msgs::robot_command temp;
-			AliceStructs::vel tempVel = it->second.generateComp(all_ideals); //Uses all of the ideals to generate compromises
+			AliceStructs::vel tempVel = it->second.generateVel(); //Uses all of the ideals to generate compromises
 			temp.rid = it->second.name;
 
 			if (tempVel.mag > 1) //caps the speed
@@ -80,6 +59,6 @@ int main(int argc, char **argv)
 		ros::spinOnce();
 		loopRate.sleep();
 
-	} */
+	}
 	return 0;
 }
