ab7248eafcfeb56915c9cde7d76934146251c21a
Henry <hlv0002@mix.wvu.edu>
2019-07-08 13:15:56 -0400

    More moving tests

--- swarm_ws/src/contour_node/src/mapping.cpp
+++ swarm_ws/src/contour_node/src/mapping.cpp
@@ -20,35 +20,38 @@ geometry_msgs::Point leftProjected, rightProjected;
 
 // Find x,y where the line passing between alpha and beta intercepts an xy plane at z
 geometry_msgs::Point findZIntercept(geometry_msgs::Point _alpha,
-        geometry_msgs::Point _beta, double _zed)
+		geometry_msgs::Point _beta, double _zed)
 {
-    /* THEORY
-     * Equation of line: r(t) = v*t+v0
-     * Direction vector: v = (xa - xb, ya - yb, za - zb)
-     * Offset vector: v0 = (xa, ya, za)
-     * Plug in z to r(t), solve for t, use t to solve for x and y/
-     */
-    
-    geometry_msgs::Point ret;
-    
-    // Check if no solution
-    if(_alpha.z == _beta.z) {
-        printf("\033[1;31mhand_pointer: \033[0;31mNo solution for intercept\033[0m\n");
-        ret.x = 0.0;
-        ret.y = 0.0;
-        ret.z = 0.0;
-    }
-    else {
-        double t = (_zed - _alpha.z) / (_alpha.z - _beta.z);
-        double x = _alpha.x * (t + 1) - _beta.x * t;
-        double y = _alpha.y * (t + 1) - _beta.y * t;
+	/* THEORY
+	 * Equation of line: r(t) = v*t+v0
+	 * Direction vector: v = (xa - xb, ya - yb, za - zb)
+	 * Offset vector: v0 = (xa, ya, za)
+	 * Plug in z to r(t), solve for t, use t to solve for x and y/
+	 */
 
-        ret.x = x;
-        ret.y = y;
-        ret.z = _zed;
-    }
-    
-    return ret;
+	geometry_msgs::Point ret;
+
+	// Check if no solution
+	if (_alpha.z == _beta.z)
+	{
+		printf(
+				"\033[1;31mhand_pointer: \033[0;31mNo solution for intercept\033[0m\n");
+		ret.x = 0.0;
+		ret.y = 0.0;
+		ret.z = 0.0;
+	}
+	else
+	{
+		double t = (_zed - _alpha.z) / (_alpha.z - _beta.z);
+		double x = _alpha.x * (t + 1) - _beta.x * t;
+		double y = _alpha.y * (t + 1) - _beta.y * t;
+
+		ret.x = x;
+		ret.y = y;
+		ret.z = _zed;
+	}
+
+	return ret;
 }
 
 static wvu_swarm_std_msgs::map_levels overall_map;
@@ -65,7 +68,8 @@ void newObs(wvu_swarm_std_msgs::obstacle obs)
 	}
 	bool found = false;
 
-	for (size_t i = 0; i < overall_map.levels.at(obs.level).functions.size() && !found; i++)
+	for (size_t i = 0;
+			i < overall_map.levels.at(obs.level).functions.size() && !found; i++)
 	{
 		if (overall_map.levels.at(obs.level).functions.at(i).name.compare(
 				obs.characteristic.name) == 0)
@@ -91,12 +95,12 @@ void newObs(wvu_swarm_std_msgs::obstacle obs)
 
 void nuiCallback(wvu_swarm_std_msgs::nuitrack_data nui)
 {
-    // Copy the message
-    g_nui = nui;
-    
-    // Find projections of hands onto table
-    leftProjected = findZIntercept(g_nui.leftWrist, g_nui.leftHand, 0.0);
-    rightProjected = findZIntercept(g_nui.rightWrist, g_nui.rightHand, 0.0);
+	// Copy the message
+	g_nui = nui;
+
+	// Find projections of hands onto table
+	leftProjected = findZIntercept(g_nui.leftWrist, g_nui.leftHand, 0.0);
+	rightProjected = findZIntercept(g_nui.rightWrist, g_nui.rightHand, 0.0);
 }
 
 int main(int argc, char **argv)
@@ -107,7 +111,7 @@ int main(int argc, char **argv)
 	ros::Publisher map_pub = n.advertise < wvu_swarm_std_msgs::map_levels
 			> ("/map_data", 1000);
 	ros::Subscriber n_obs = n.subscribe("/add_obstacle", 1000, newObs);
-        ros::Subscriber nuiSub = n.subscribe("/nuitrack_bridge", 1000, nuiCallback);
+	ros::Subscriber nuiSub = n.subscribe("/nuitrack_bridge", 1000, nuiCallback);
 
 	///////////////////////////////////////////////////////////////////
 	// default map setup
@@ -134,18 +138,18 @@ int main(int argc, char **argv)
 
 	newObs(obs);
 
-	el.x_rad = 7;
-	el.y_rad = 4;
+	el.x_rad = 4;
+	el.y_rad = 7;
 	el.theta_offset = 0;
 
 	gaus.ellipse = el;
-	gaus.ellipse.offset_x = 5;
+	gaus.ellipse.offset_x = 10;
 	gaus.ellipse.offset_y = 0;
 	gaus.amplitude = 10;
 	gaus.name = "Jeff";
 
 	obs.characteristic = gaus;
-	obs.level = map_ns::OBSTACLE;
+	obs.level = map_ns::TARGET;
 
 	newObs(obs);
 #endif
@@ -157,8 +161,31 @@ int main(int argc, char **argv)
 
 	ros::Rate rate(60);
 
+#if TEST_EQU
+	int tick = 0;
+#endif
+
 	while (ros::ok())
 	{
+#if TEST_EQU
+		tick++;
+		tick %= 1000;
+
+		el.x_rad = 5;
+		el.y_rad = 2;
+		el.theta_offset = tick * M_PI / 100;
+
+		gaus.ellipse = el;
+		gaus.ellipse.offset_x = 0;
+		gaus.ellipse.offset_y = 0;
+		gaus.amplitude = 20;
+		gaus.name = "Bob";
+
+		obs.characteristic = gaus;
+		obs.level = map_ns::TARGET;
+		newObs(obs);
+#endif
+
 		map_pub.publish(overall_map);
 
 		ros::spinOnce();
