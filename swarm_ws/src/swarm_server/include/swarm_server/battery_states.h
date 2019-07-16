c2d439118cda880bed1c7d1858163543634be6ca
nhewitt99 <nhewitt@uncc.edu>
2019-07-16 17:10:57 -0400

    Forgot this, whoops

---
+++ swarm_ws/src/swarm_server/include/swarm_server/battery_states.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   battery_states.h
+ * Author: Henry Vos
+ *
+ * Created on July 16, 2019, 4:39 PM
+ */
+
+#ifndef BATTERY_STATES_H
+#define BATTERY_STATES_H
+
+enum BATTERY_STATES
+{
+    ERROR = -2,
+    NONE = -1,
+    CHARGED,
+    GOING,
+    CHARGING
+};
+
+typedef enum BATTERY_STATES BatState;
+
+#endif /* BATTERY_STATES_H */
+
