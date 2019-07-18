793bcc8ff53596f13fd6e65e434bff62c22a55ef
Henry <hlv0002@mix.wvu.edu>
2019-07-18 16:02:35 -0400

    Can get dot to screen

--- swarm_ws/src/visualization/include/visualization/keyboard_managment.h
+++ swarm_ws/src/visualization/include/visualization/keyboard_managment.h
@@ -6,14 +6,19 @@
 
 #include <wvu_swarm_std_msgs/map_levels.h>
 
+#include <contour_node/level_description.h>
+
 #include "visualization_settings.h"
 #include "perspective_transform_gpu.h"
+
+#define USING_LEVEL map_ns::TARGET
 
 namespace interaction
 {
 
-ros::Publisher add_pub, rem_pub;
+ros::Publisher add_pub, rem_pub, loc_pub;
 wvu_swarm_std_msgs::map_levels *universe;
+quadrilateral_t table;
 
 sf::Vector2f getMouseCordinate(sf::Vector2f screen_loc, quadrilateral_t quad);
 
@@ -24,6 +29,8 @@ void mouseReleasedEvent(sf::Event e);
 void mouseMovedEvent(sf::Event e);
 
 void scrollWheelMoved(sf::Event e);
+
+void drawSelection(sf::RenderTexture *window);
 
 } // interaction
 
--- swarm_ws/src/visualization/src/keyboard_managment.cpp
+++ swarm_ws/src/visualization/src/keyboard_managment.cpp
@@ -1,6 +1,23 @@
 #include <visualization/keyboard_managment.h>
+#include <std_msgs/String.h>
+#include <wvu_swarm_std_msgs/obstacle.h>
+#include <geometry_msgs/Point.h>
 
-#define scale(val, n_min, n_max, o_min, o_max) (((val - o_min) / (o_max - o_min)) * (n_max - n_min) + n_min)
+#include <chrono>
+
+using namespace std::chrono;
+
+#define scale(val, o_min, o_max, n_min, n_max) (((val - o_min) / (o_max - o_min)) * (n_max - n_min) + n_min)
+
+static wvu_swarm_std_msgs::obstacle *g_selected;
+static double x, y;
+
+enum MODE
+{
+	None, Rad_x, Rad_y, Theta_off, Amplitude
+};
+
+static MODE editMode = None;
 
 sf::Vector2f interaction::getMouseCordinate(sf::Vector2f initial,
 		quadrilateral_t trap)
@@ -28,16 +45,86 @@ sf::Vector2f interaction::getMouseCordinate(sf::Vector2f initial,
 	double unified_y =
 			left.y != right.y ? (m0 * (unified_x - right.x) + right.y) : left.y;
 
-	return sf::Vector2f((float) unified_x, (float) unified_y);
+	unified_x *= 200.0 / (double)WIDTH;
+	unified_y *= 100.0 / (double)HEIGHT;
+	unified_x -= 100;
+	unified_y -= 50;
+	unified_y *= -1;
+//	unified_x *= -1;
+
+	return sf::Vector2f((float) unified_y, (float) unified_x);
+}
+
+void addNewFunk()
+{
+	wvu_swarm_std_msgs::ellipse ellip;
+	ellip.x_rad = 5;
+	ellip.y_rad = 5;
+	ellip.offset_x = x;
+	ellip.offset_y = y;
+	ellip.theta_offset = 0;
+
+	wvu_swarm_std_msgs::gaussian charac;
+	charac.amplitude = 10;
+	charac.ellipse = ellip;
+	// creating an arbitrary name
+	charac.name = "funk_"
+			+ std::to_string(
+					(long) duration_cast < milliseconds
+							> (high_resolution_clock::now().time_since_epoch()).count());
+	charac.selected = false;
+
+	wvu_swarm_std_msgs::obstacle n_obs;
+	n_obs.characteristic = charac;
+	n_obs.level = USING_LEVEL;
+
+	interaction::add_pub.publish(n_obs);
 }
 
 void interaction::keyEvent(sf::Event e)
 {
-	switch (e.key.code)
+	if (g_selected != NULL)
 	{
-	case sf::Keyboard::Delete:
+		switch (e.key.code)
+		{
+		case sf::Keyboard::Delete: // removing selected function
+			std_msgs::String str;
+			str.data = g_selected->characteristic.name;
+			interaction::rem_pub.publish(str);
+			ROS_INFO("Removed: %s", str.data.c_str());
+			break;
+		}
+	}
+	else
+	{
+		switch (e.key.code)
+		{
+		case sf::Keyboard::Key::Return: // adding new function
+			addNewFunk();
+			break;
 
-		break;
+		case sf::Keyboard::Key::T:
+			editMode = Theta_off;
+			break;
+
+		case sf::Keyboard::Key::Y:
+			editMode = Rad_y;
+			break;
+
+		case sf::Keyboard::Key::X:
+			editMode = Rad_x;
+			break;
+
+		case sf::Keyboard::Key::A:
+			editMode = Amplitude;
+			break;
+
+		case sf::Keyboard::Key::Space:
+		case sf::Keyboard::Key::Escape:
+			editMode = None;
+			g_selected = NULL;
+			break;
+		}
 	}
 }
 
@@ -53,10 +140,26 @@ void interaction::mouseReleasedEvent(sf::Event e)
 
 void interaction::mouseMovedEvent(sf::Event e)
 {
+	sf::Vector2f mloc = interaction::getMouseCordinate(
+			sf::Vector2f(e.mouseMove.x, e.mouseMove.y), interaction::table);
 
+	x = mloc.x;
+	y = mloc.y;
+
+	geometry_msgs::Point pnt;
+	pnt.x = x;
+	pnt.y = y;
+	pnt.z = 0;
+
+	interaction::loc_pub.publish(pnt);
 }
 
 void interaction::scrollWheelMoved(sf::Event e)
+{
+
+}
+
+void interaction::drawSelection(sf::RenderTexture *window)
 {
 
 }
