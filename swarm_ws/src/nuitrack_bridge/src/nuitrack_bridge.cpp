0ba58bc4682e712849a5c67195223b7458d28829
Henry <hlv0002@mix.wvu.edu>
2019-07-19 09:00:11 -0400

    Added CSV logging in NUI track node

--- swarm_ws/src/nuitrack_bridge/src/nuitrack_bridge.cpp
+++ swarm_ws/src/nuitrack_bridge/src/nuitrack_bridge.cpp
@@ -33,248 +33,282 @@
 
 #define PORT 8080
 
+#define OUTPUT_TO_CSV 1
+
+#if OUTPUT_TO_CSV
+#include <fstream>
+#endif
+
 bool g_sigint_received = false;
 
 void flagger(int sig)
 {
-    g_sigint_received = true;
+	g_sigint_received = true;
 }
 
 // Function to transform a single xyz
 void xyzTf(xyz &_xyz, tf::TransformListener &tfLis)
 {
-    // Create a geometry message point because tf needs it
-    geometry_msgs::PointStamped pt;
-    pt.header.frame_id = "kinect";
-    pt.header.stamp = ros::Time();
-    pt.point.x = _xyz.x / 1000; // mm to meters
-    pt.point.y = _xyz.y / 1000;
-    pt.point.z = _xyz.z / 1000;
+	// Create a geometry message point because tf needs it
+	geometry_msgs::PointStamped pt;
+	pt.header.frame_id = "kinect";
+	pt.header.stamp = ros::Time();
+	pt.point.x = _xyz.x / 1000; // mm to meters
+	pt.point.y = _xyz.y / 1000;
+	pt.point.z = _xyz.z / 1000;
 
-    try {
-        // Another placeholder point to transform into
-        geometry_msgs::PointStamped ptTf;
+	try
+	{
+		// Another placeholder point to transform into
+		geometry_msgs::PointStamped ptTf;
 
-        // Find transform from kinect to world
-        tfLis.waitForTransform("kinect", "world", ros::Time(), ros::Duration(0.01));
-        tfLis.transformPoint("world", pt, ptTf); // Transform pt into world, store in ptTf
+		// Find transform from kinect to world
+		tfLis.waitForTransform("kinect", "world", ros::Time(), ros::Duration(0.01));
+		tfLis.transformPoint("world", pt, ptTf); // Transform pt into world, store in ptTf
 
-        // Apply to input
-        _xyz.x = ptTf.point.x * 100; // meters to cm
-        _xyz.y = ptTf.point.y * 100;
-        _xyz.z = ptTf.point.z * 100;
-    }
-    catch (tf::TransformException &ex) {
-        ROS_ERROR("Transform error! %s", ex.what());
-    }
+		// Apply to input
+		_xyz.x = ptTf.point.x * 100; // meters to cm
+		_xyz.y = ptTf.point.y * 100;
+		_xyz.z = ptTf.point.z * 100;
+	} catch (tf::TransformException &ex)
+	{
+		ROS_ERROR("Transform error! %s", ex.what());
+	}
 }
 
 // Function to transform an nuiData struct from Kinect to global frame
 void nuiTf(nuiData &_nui, tf::TransformListener &tfLis)
 {
-    // Transform each xyz point
-    if(_nui.leftFound) {
-        xyzTf(_nui.leftHand, tfLis);
-        xyzTf(_nui.leftWrist, tfLis);
-    }
-    if(_nui.rightFound) {
-        xyzTf(_nui.rightHand, tfLis);
-        xyzTf(_nui.rightWrist, tfLis);
-    }
+	// Transform each xyz point
+	if (_nui.leftFound)
+	{
+		xyzTf(_nui.leftHand, tfLis);
+		xyzTf(_nui.leftWrist, tfLis);
+	}
+	if (_nui.rightFound)
+	{
+		xyzTf(_nui.rightHand, tfLis);
+		xyzTf(_nui.rightWrist, tfLis);
+	}
 }
 
 // Generates a visualizable Marker from an xyz struct
-visualization_msgs::Marker xyzToMarker(int _id, xyz *_xyz, double _r, double _g, double _b, double _a = 1.0, double _size = 4)
+visualization_msgs::Marker xyzToMarker(int _id, xyz *_xyz, double _r, double _g,
+		double _b, double _a = 1.0, double _size = 4)
 {
-    visualization_msgs::Marker ret;
+	visualization_msgs::Marker ret;
 
-    ret.header.stamp = ros::Time(); // Metadata
-    ret.header.frame_id = "world";
-    ret.ns = "nui";
-    ret.id = _id; // Unique identifier
-    ret.type = visualization_msgs::Marker::SPHERE;
-    ret.pose.position.x = _xyz->x; // Measured in meters
-    ret.pose.position.y = _xyz->y;
-    ret.pose.position.z = _xyz->z;
-    ret.pose.orientation.x = 0; // Quaternion
-    ret.pose.orientation.y = 0;
-    ret.pose.orientation.z = 0;
-    ret.pose.orientation.w = 1;
-    ret.scale.x = _size; // Measured in meters
-    ret.scale.y = _size;
-    ret.scale.z = _size;
-    ret.color.r = _r;
-    ret.color.g = _g;
-    ret.color.b = _b;
-    ret.color.a = _a;
+	ret.header.stamp = ros::Time(); // Metadata
+	ret.header.frame_id = "world";
+	ret.ns = "nui";
+	ret.id = _id; // Unique identifier
+	ret.type = visualization_msgs::Marker::SPHERE;
+	ret.pose.position.x = _xyz->x; // Measured in meters
+	ret.pose.position.y = _xyz->y;
+	ret.pose.position.z = _xyz->z;
+	ret.pose.orientation.x = 0; // Quaternion
+	ret.pose.orientation.y = 0;
+	ret.pose.orientation.z = 0;
+	ret.pose.orientation.w = 1;
+	ret.scale.x = _size; // Measured in meters
+	ret.scale.y = _size;
+	ret.scale.z = _size;
+	ret.color.r = _r;
+	ret.color.g = _g;
+	ret.color.b = _b;
+	ret.color.a = _a;
 
-    return ret;
+	return ret;
 }
 
 // Generates a simple Point message from an xyz struct
 geometry_msgs::Point xyzToPoint(xyz *_xyz)
 {
-    geometry_msgs::Point ret;
+	geometry_msgs::Point ret;
 
-    ret.x = _xyz->x;
-    ret.y = _xyz->y;
-    ret.z = _xyz->z;
+	ret.x = _xyz->x;
+	ret.y = _xyz->y;
+	ret.z = _xyz->z;
 
-    return ret;
+	return ret;
 }
 
 // Generates an array to convert an entire nuiData struct into visualizable form
 visualization_msgs::MarkerArray nuiToMarkers(nuiData *_nui)
 {
-    visualization_msgs::MarkerArray ret;
+	visualization_msgs::MarkerArray ret;
 
-    // Use confidence values as alpha-- less certain joints will be more transparent
-    double leftHandAlpha = 0.5; //_nui->confLH;
-    double leftWristAlpha = 0.5; //_nui->confLW;
-    double rightHandAlpha = 0.5; //_nui->confRH;
-    double rightWristAlpha = 0.5; //_nui->confRW;
+	// Use confidence values as alpha-- less certain joints will be more transparent
+	double leftHandAlpha = 0.5; //_nui->confLH;
+	double leftWristAlpha = 0.5; //_nui->confLW;
+	double rightHandAlpha = 0.5; //_nui->confRH;
+	double rightWristAlpha = 0.5; //_nui->confRW;
 
-    ret.markers.push_back(xyzToMarker(1, &(_nui->leftWrist), 1, 0.5, 0, leftWristAlpha));
-    ret.markers.push_back(xyzToMarker(2, &(_nui->leftHand), 1, 0, 0, leftHandAlpha, _nui->leftClick ? 6 : 4));
-    ret.markers.push_back(xyzToMarker(3, &(_nui->rightWrist), 0, 0.5, 1, rightWristAlpha));
-    ret.markers.push_back(xyzToMarker(4, &(_nui->rightHand), 0, 0, 1, rightHandAlpha, _nui->rightClick ? 6 : 4));
+	ret.markers.push_back(
+			xyzToMarker(1, &(_nui->leftWrist), 1, 0.5, 0, leftWristAlpha));
+	ret.markers.push_back(
+			xyzToMarker(2, &(_nui->leftHand), 1, 0, 0, leftHandAlpha,
+					_nui->leftClick ? 6 : 4));
+	ret.markers.push_back(
+			xyzToMarker(3, &(_nui->rightWrist), 0, 0.5, 1, rightWristAlpha));
+	ret.markers.push_back(
+			xyzToMarker(4, &(_nui->rightHand), 0, 0, 1, rightHandAlpha,
+					_nui->rightClick ? 6 : 4));
 
-    return ret;
+	return ret;
 }
 
 // Generates an nuitrack_data ROS message from an nuiData struct
 wvu_swarm_std_msgs::nuitrack_data nuiToRos(nuiData *_nui)
 {
-    wvu_swarm_std_msgs::nuitrack_data ret;
+	wvu_swarm_std_msgs::nuitrack_data ret;
 
-    ret.leftWrist = xyzToPoint(&(_nui->leftWrist));
-    ret.leftHand = xyzToPoint(&(_nui->leftHand));
-    ret.rightWrist = xyzToPoint(&(_nui->rightWrist));
-    ret.rightHand = xyzToPoint(&(_nui->rightHand));
-    ret.gestureData = (char)(_nui->gestureData);
-    ret.leftClick = _nui->leftClick;
-    ret.rightClick = _nui->rightClick;
+	ret.leftWrist = xyzToPoint(&(_nui->leftWrist));
+	ret.leftHand = xyzToPoint(&(_nui->leftHand));
+	ret.rightWrist = xyzToPoint(&(_nui->rightWrist));
+	ret.rightHand = xyzToPoint(&(_nui->rightHand));
+	ret.gestureData = (char) (_nui->gestureData);
+	ret.leftClick = _nui->leftClick;
+	ret.rightClick = _nui->rightClick;
 
-    return ret;
+	return ret;
 }
 
-int main(int argc, char** argv) {
-    // ROS setup
-    ros::init(argc, argv, "nuitrack_bridge");
+int main(int argc, char **argv)
+{
+	// ROS setup
+	ros::init(argc, argv, "nuitrack_bridge");
 
-    // Generates nodehandles, publisher
-    ros::NodeHandle n;
-    ros::NodeHandle n_priv("~"); // private handle
-    ros::Rate rate(50);
-    ros::Publisher pubVis, pubNui;
-    pubVis = n.advertise<visualization_msgs::MarkerArray>("visualization", 1000);
-    pubNui = n.advertise<wvu_swarm_std_msgs::nuitrack_data>("unfiltered", 1000);
+	// Generates nodehandles, publisher
+	ros::NodeHandle n;
+	ros::NodeHandle n_priv("~"); // private handle
+	ros::Rate rate(50);
+	ros::Publisher pubVis, pubNui;
+	pubVis = n.advertise < visualization_msgs::MarkerArray
+			> ("visualization", 1000);
+	pubNui = n.advertise < wvu_swarm_std_msgs::nuitrack_data
+			> ("unfiltered", 1000);
 
-    // Transform listener
-    tf::TransformListener tfLis;
+	// Transform listener
+	tf::TransformListener tfLis;
 
-    // Set up signal handler
-    signal(SIGINT, flagger);
+	// Set up signal handler
+	signal(SIGINT, flagger);
 
-    /* Server setup */
-    int sockfd;
-    struct sockaddr_in servaddr, cliaddr;
+	/* Server setup */
+	int sockfd;
+	struct sockaddr_in servaddr, cliaddr;
 
-    // Attempt to create the socket file descriptor
-    if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        std::cerr << "Socket creation failed!" << std::endl;
-        return -1;
-    }
+	// Attempt to create the socket file descriptor
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
+		std::cerr << "Socket creation failed!" << std::endl;
+		return -1;
+	}
 
-    // Fill the data structures with server information
-    servaddr.sin_family = AF_INET; // IPv4
-    servaddr.sin_addr.s_addr = INADDR_ANY; // Accept messages from any address
-    servaddr.sin_port = htons(PORT); // Sets port number, converts byte order
-    /* End server setup */
+	// Fill the data structures with server information
+	servaddr.sin_family = AF_INET; // IPv4
+	servaddr.sin_addr.s_addr = INADDR_ANY; // Accept messages from any address
+	servaddr.sin_port = htons(PORT); // Sets port number, converts byte order
+	/* End server setup */
 
-    char send = '\0';
+	char send = '\0';
 
-    nuiData nui = nuiData();
+	nuiData nui = nuiData();
 
-    while(!g_sigint_received && ros::ok())
-    {
-        /* Send to server */
-        send = 'a'; // For now, can send anything and server will respond.
-        if(sendto(sockfd, &send, sizeof(send), MSG_CONFIRM,
-                (const struct sockaddr*)&servaddr, sizeof(servaddr)) >= 0)
-        {
+	while (!g_sigint_received && ros::ok())
+	{
+		/* Send to server */
+		send = 'a'; // For now, can send anything and server will respond.
+		if (sendto(sockfd, &send, sizeof(send), MSG_CONFIRM,
+				(const struct sockaddr*) &servaddr, sizeof(servaddr)) >= 0)
+		{
 //            std::cout << "Sent " << send << " to server." << std::endl;
-        }
-        else
-        {
-            std::cout << "Error in sending!" << std::endl;
-        }
-        /* End send to server */
+		}
+		else
+		{
+			std::cout << "Error in sending!" << std::endl;
+		}
+		/* End send to server */
 
-        /* Wait for response */
-        // Set up socket to watch in select()
-        fd_set rfds;
-        FD_ZERO(&rfds);
-        FD_SET(sockfd, &rfds);
+		/* Wait for response */
+		// Set up socket to watch in select()
+		fd_set rfds;
+		FD_ZERO(&rfds);
+		FD_SET(sockfd, &rfds);
 
-        // Set timeout to 10 ms
-        struct timeval timeout;
-        timeout.tv_sec = 1;
-        timeout.tv_usec = 0;//10000;
+		// Set timeout to 10 ms
+		struct timeval timeout;
+		timeout.tv_sec = 1;
+		timeout.tv_usec = 0; //10000;
 
-        // Check if socket is available (non-blocking) for up to timeout seconds
-        int recVal = select(sockfd + 2, &rfds, NULL, NULL, &timeout);
-        switch(recVal)
-        {
-            case(0):
-            {
-                //Timeout
-                std::cout << "Timeout on socket." << std::endl;
-                break;
-            }
-            case(-1):
-            {
-                //Error
-                std::cout << "Select returned error!" << std::endl;
-                break;
-            }
-            default:
-            {
-                // Data is available!
+		// Check if socket is available (non-blocking) for up to timeout seconds
+		int recVal = select(sockfd + 2, &rfds, NULL, NULL, &timeout);
+		switch (recVal)
+		{
+		case (0):
+		{
+			//Timeout
+			std::cout << "Timeout on socket." << std::endl;
+			break;
+		}
+		case (-1):
+		{
+			//Error
+			std::cout << "Select returned error!" << std::endl;
+			break;
+		}
+		default:
+		{
+			// Data is available!
 //                std::cout << "Socket is available." << std::endl;
 
-                int len, n;
-                nuiData rec = nuiData();
+			int len, n;
+			nuiData rec = nuiData();
 
-                // Data was found, receive it from the socket
-                n = recvfrom(sockfd, &rec, sizeof(rec), MSG_WAITALL,
-                        (struct sockaddr*)&cliaddr, (socklen_t*)&len);
+			// Data was found, receive it from the socket
+			n = recvfrom(sockfd, &rec, sizeof(rec), MSG_WAITALL,
+					(struct sockaddr*) &cliaddr, (socklen_t*) &len);
 
-                // Store in global
-                nui = rec;
+			// Store in global
+			nui = rec;
 
-                // Transform global
-                nuiTf(nui, tfLis);
+			// Transform global
+			nuiTf(nui, tfLis);
 
-                break;
-            }
-        }
-        /* End wait for response */
+			break;
+		}
+		}
+		/* End wait for response */
 
-        // Create and publish visualizable message for rviz
-        visualization_msgs::MarkerArray visOut = nuiToMarkers(&nui);
-        pubVis.publish(visOut);
+		// Create and publish visualizable message for rviz
+		visualization_msgs::MarkerArray visOut = nuiToMarkers(&nui);
+		pubVis.publish(visOut);
 
-        // Create and publish nutrack_data so other nodes can interpret
-        wvu_swarm_std_msgs::nuitrack_data nuiOut = nuiToRos(&nui);
-        pubNui.publish(nuiOut);
+		// Create and publish nutrack_data so other nodes can interpret
+		wvu_swarm_std_msgs::nuitrack_data nuiOut = nuiToRos(&nui);
+		pubNui.publish(nuiOut);
 
-        ros::spinOnce();
-        rate.sleep();
-    }
+#if OUTPUT_TO_CSV
+		std::ofstream file;
+		file.open("/home/air/tracking_log.csv", std::ios::out | std::ios::app);
+		// writing to file
+		// line order
+		// [Left wrist x, y, z], [Left hand x, y, z], [Right wrist x, y, z], [Right hand x, y, z]
+		file << nuiOut.leftWrist.x << "," << nuiOut.leftWrist.y << ","
+				<< nuiOut.leftWrist.z << "," << nuiOut.leftHand.x << ","
+				<< nuiOut.leftHand.y << "," << nuiOut.leftHand.z << ","
+				<< nuiOut.rightWrist.x << "," << nuiOut.rightWrist.y << ","
+				<< nuiOut.rightWrist.z << "," << nuiOut.rightHand.x << ","
+				<< nuiOut.rightHand.y << "," << nuiOut.rightHand.z << std::endl;
+		file.close();
+#endif
+		ros::spinOnce();
+		rate.sleep();
+	}
 
-    close(sockfd);
-    std::cout << "Closed socket." << std::endl;
+	close(sockfd);
+	std::cout << "Closed socket." << std::endl;
 
-    return 0;
+	return 0;
 }
